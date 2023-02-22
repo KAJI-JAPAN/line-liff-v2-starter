fe65cd0c46898d2eed11d22f18e4fbed07b92a2e
KAJI-JAPAN <kurowassan1206@gmail.com>
2023-02-22 12:10:49 +0900

    add newTodo

--- src/nextjs/pages/index.js
+++ src/nextjs/pages/index.js
@@ -1,7 +1,16 @@
 import Head from "next/head";
+import { useState } from "react";
 import packageJson from "../package.json";
 
 export default function Home(props) {
+  const [newTodoToggle, setNewTodoToggle] = useState(false);
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState([]);
+
+  const newTodoSave = () => {
+    setResult(input)
+  }
+  console.log(result)
   /** You can access to liff and liffError object through the props.
    *  const { liff, liffError } = props;
    *  console.log(liff.getVersion());
@@ -11,62 +20,17 @@ export default function Home(props) {
   return (
     <div>
       <Head>
-        <title>LIFF Starter</title>
+        <title>Todo Sample</title>
       </Head>
-      <div className="home">
-        <h1 className="home__title">
-          Welcome to <br />
-          <a
-            className="home__title__link"
-            href="https://developers.line.biz/en/docs/liff/overview/"
-          >
-            LIFF Starter!
-          </a>
-        </h1>
-        <div className="home__badges">
-          <span className="home__badges__badge badge--primary">
-            LIFF Starter
-          </span>
-          <span className="home__badges__badge badge--secondary">nextjs</span>
-          <span className="home__badges__badge badge--primary">
-            {packageJson.version}
-          </span>
-          <a
-            href="https://github.com/line/line-liff-v2-starter"
-            target="_blank"
-            rel="noreferrer"
-            className="home__badges__badge badge--secondary"
-          >
-            GitHub
-          </a>
+        <button onClick={() => setNewTodoToggle(!newTodoToggle)}>＋ New Todo</button>
+        {newTodoToggle &&
+          <div>
+          Todo Title: <input value={input} onChange={(event) => setInput(event.target.value)} />
+          <button>x</button>
+          <textarea rows={7} cols={30}></textarea>
+          <button onClick={newTodoSave}>Save</button>
         </div>
-        <div className="home__buttons">
-          <a
-            href="https://developers.line.biz/en/docs/liff/developing-liff-apps/"
-            target="_blank"
-            rel="noreferrer"
-            className="home__buttons__button button--primary"
-          >
-            LIFF Documentation
-          </a>
-          <a
-            href="https://liff-playground.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-            className="home__buttons__button button--tertiary"
-          >
-            LIFF Playground
-          </a>
-          <a
-            href="https://developers.line.biz/console/"
-            target="_blank"
-            rel="noreferrer"
-            className="home__buttons__button button--secondary"
-          >
-            LINE Developers Console
-          </a>
-        </div>
-      </div>
+          }
     </div>
   );
 }
